ee1cb141e58c3463c5cbf79cce821ee7d380134a
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2023-03-15 15:29:42 +0200

    Small fix

--- server/todo-server/src/auth/strategies/jwt.strategy.ts
+++ server/todo-server/src/auth/strategies/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExparation: true,
+      ignoreExpiration: true,
       secretOrKey: process.env.JWT_SECRET,
     });
   }
