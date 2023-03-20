53c9a0790bb0df3d4cfc5d59d0df407f76bf7476
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2023-03-20 06:07:12 +0200

    Completed unit tests for auth and todo

--- server/todo-server/src/auth/auth.service.spec.ts
+++ server/todo-server/src/auth/auth.service.spec.ts
@@ -1,18 +1,143 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/sequelize';
+import { JwtService } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserModel } from './user.model';
+import { AuthDto } from './dto/auth.dto';
+import { BadRequestException, UnauthorizedException } from '@nestjs/common';
+import {
+  ALREADY_REGISTERED_ERROR,
+  USER_NOT_FOUND_ERROR,
+  WRONG_PASSWORD_ERROR,
+} from './auth.constants';
+
+const userModelMock = {
+  findOne: jest.fn(),
+  create: jest.fn(),
+};
+
+const jwtServiceMock = {
+  signAsync: jest.fn(),
+};
+
+userModelMock.findOne.mockImplementation(async (options) => {
+  if (options.where.email === 'existing@example.com') {
+    return {
+      id: 1,
+      email: 'existing@example.com',
+      passwordHash: 'hashedpassword',
+    };
+  }
+  return null;
+});
+
+userModelMock.create.mockImplementation(async (userData) => {
+  return {
+    id: 2,
+    email: userData.email,
+    passwordHash: userData.passwordHash,
+    createdAt: userData.createdAt,
+    updatedAt: userData.updatedAt,
+  };
+});
+
+jwtServiceMock.signAsync.mockImplementation(async () => {
+  return 'sample_access_token';
+});
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: getModelToken(UserModel), useValue: userModelMock }, // замена зависимости на мок
+        { provide: JwtService, useValue: jwtServiceMock }, // замена зависимости на мок
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should successfully login a user', async () => {
+    const testDto: AuthDto = {
+      login: 'existing@example.com',
+      password: 'correctpassword',
+    };
+
+    const result = await service.login(testDto.login);
+
+    expect(jwtServiceMock.signAsync).toHaveBeenCalledWith({
+      email: testDto.login,
+    });
+    expect(result).toEqual({ access_token: 'sample_access_token' });
+  });
+
+  it('should fail to login a non-existent user', async () => {
+    const testDto: AuthDto = {
+      login: 'nonexistent@example.com',
+      password: 'somepassword',
+    };
+
+    await expect(
+      service.validateUser(testDto.login, testDto.password),
+    ).rejects.toThrow(UnauthorizedException);
+    await expect(
+      service.validateUser(testDto.login, testDto.password),
+    ).rejects.toMatchObject({
+      message: USER_NOT_FOUND_ERROR,
+    });
+  });
+
+  it('should successfully create a user', async () => {
+    const testDto: AuthDto = {
+      login: 'newuser@example.com',
+      password: 'newpassword',
+    };
+
+    const newUser = await service.createUser(testDto);
+
+    expect(userModelMock.create).toHaveBeenCalled();
+    expect(newUser.email).toBe(testDto.login);
+    expect(newUser.passwordHash).not.toBe(testDto.password);
+  });
+
+  it('should throw BadRequestException if the user already exists', async () => {
+    const testDto: AuthDto = {
+      login: 'existing@example.com',
+      password: 'somepassword',
+    };
+
+    await expect(service.createUser(testDto)).rejects.toThrow(
+      BadRequestException,
+    );
+    await expect(service.createUser(testDto)).rejects.toMatchObject({
+      message: ALREADY_REGISTERED_ERROR,
+    });
+  });
+
+  it('should fail to login with incorrect password', async () => {
+    const testDto: AuthDto = {
+      login: 'existing@example.com',
+      password: 'wrongpassword',
+    };
+
+    await expect(
+      service.validateUser(testDto.login, testDto.password),
+    ).rejects.toThrow(UnauthorizedException);
+    await expect(
+      service.validateUser(testDto.login, testDto.password),
+    ).rejects.toMatchObject({
+      message: WRONG_PASSWORD_ERROR,
+    });
   });
 });
