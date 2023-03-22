d38b60dadce05df64f426597585206727d53b3f7
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2023-03-22 19:09:20 +0200

    Create protected route

--- client/todo-client/src/App.tsx
+++ client/todo-client/src/App.tsx
@@ -2,14 +2,16 @@ import { Route, Routes, Link, BrowserRouter } from "react-router-dom";
 import HomePage from "./components/MainPage";
 import LoginPage from "./components/LoginPage";
 import MainPage from "./components/MainPage";
+import PrivateRoutes from "./utils/PrivateRoute";
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<HomePage />} />
+                <Route path="/" element={<PrivateRoutes />}>
+                    <Route path="/main" element={<MainPage />} />
+                </Route>
                 <Route path="/login" element={<LoginPage />} />
-                <Route path="/main" element={<MainPage />} />
             </Routes>
         </BrowserRouter>
     );
