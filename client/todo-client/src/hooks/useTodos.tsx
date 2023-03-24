3ae28f2c611777b0e7a041f4448c8382736ddcc0
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2023-03-24 18:32:51 +0200

    Create all CRUD methods in useTodos hook

--- client/todo-client/src/hooks/useTodos.tsx
+++ client/todo-client/src/hooks/useTodos.tsx
@@ -53,15 +53,115 @@ const useTodos = () => {
     };
 
     const updateTodoCompletion = async (id: number, completed: boolean) => {
-        // Implement update logic here
+        const token = localStorage.getItem("access_token");
+
+        if (!token) {
+            setError(new Error("User is not authenticated"));
+            return;
+        }
+
+        const todoToUpdate = todos.find((todo) => todo.id === id);
+        if (!todoToUpdate) {
+            setError(new Error("Todo not found"));
+            return;
+        }
+
+        const updatedTodoData = {
+            ...todoToUpdate,
+            completed,
+        };
+
+        try {
+            const response = await fetch(`http://localhost:3333/todos/${id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify(updatedTodoData),
+            });
+
+            if (!response.ok) {
+                throw new Error("Failed to update todo");
+            }
+
+            const updatedTodo = await response.json();
+
+            setTodos((prevTodos) =>
+                prevTodos.map((todo) =>
+                    todo.id === updatedTodo.id ? updatedTodo : todo
+                )
+            );
+        } catch (err) {
+            setError(err as Error);
+        }
     };
 
     const removeTodo = async (id: number) => {
-        // Implement delete logic here
+        setLoading(true);
+        const token = localStorage.getItem("access_token");
+
+        if (!token) {
+            setError(new Error("User is not authenticated"));
+            setLoading(false);
+            return;
+        }
+
+        try {
+            const response = await fetch(`http://localhost:3333/todos/${id}`, {
+                method: "DELETE",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error("Failed to remove todo");
+            }
+
+            setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+        } catch (err) {
+            setError(err as Error);
+        } finally {
+            setLoading(false);
+        }
     };
 
     const addTodo = async (title: string, description: string) => {
-        // Implement create logic here
+        setLoading(true);
+        const token = localStorage.getItem("access_token");
+
+        if (!token) {
+            setError(new Error("User is not authenticated"));
+            setLoading(false);
+            return;
+        }
+
+        try {
+            const response = await fetch("http://localhost:3333/todos", {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    title: title,
+                    description: description,
+                    completed: false,
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error("Failed to add todo");
+            }
+
+            const newTodo = await response.json();
+            setTodos((prevTodos) => [...prevTodos, newTodo]);
+        } catch (err) {
+            setError(err as Error);
+        } finally {
+            setLoading(false);
+        }
     };
 
     useEffect(() => {
