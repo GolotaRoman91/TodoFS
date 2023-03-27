354629beeac63ea24ec10701608b117024aa495b
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2023-03-27 19:40:08 +0300

    Add Jest, create tests for addTodo(need fix)

---
+++ client/todo-client/jest.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "jsdom",
+    moduleNameMapper: {
+        "\\.css$": "identity-obj-proxy",
+    },
+    setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+    testMatch: ["<rootDir>/tests/**/*.test.{ts,tsx}"],
+};
