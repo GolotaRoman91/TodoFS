3c1baaa204f280664d65b3b4c8a4d787d4867341
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2023-03-30 00:27:10 +0300

    Add test for useAuth

---
+++ client/todo-client/tests/useAuth.test.ts
@@ -0,0 +1,47 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../src/services/loginUser";
+import { registerUser } from "../src/services/registerUser";
+import useAuth from "../src/hooks/useAuth";
+
+const navigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+    useNavigate: () => navigate,
+}));
+
+jest.mock("../src/services/loginUser", () => ({
+    loginUser: jest.fn(),
+}));
+
+jest.mock("../src/services/registerUser", () => ({
+    registerUser: jest.fn(),
+}));
+
+describe("useAuth hook", () => {
+    it("should toggle isLogin state", () => {
+        const { result } = renderHook(() => useAuth());
+        expect(result.current.isLogin).toBe(true);
+
+        act(() => {
+            result.current.setIsLogin(false);
+        });
+
+        expect(result.current.isLogin).toBe(false);
+    });
+
+    it("should call loginUser with correct arguments", async () => {
+        const credentials = { login: "test", password: "test" };
+        const email = "test@test.com";
+
+        const { result } = renderHook(() => useAuth());
+        await act(() => result.current.loginUser(credentials, email));
+
+        expect(loginUser).toHaveBeenCalledWith(credentials, email, navigate);
+    });
+
+    it("should have registerUser function available", () => {
+        const { result } = renderHook(() => useAuth());
+        expect(result.current.registerUser).toBe(registerUser);
+    });
+});
